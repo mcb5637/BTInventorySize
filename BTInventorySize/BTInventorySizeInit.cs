bb390184b8e5fa3f90ba59ce7aa250b1d2cfc22c
Marc Bohler <mgrbo@web.de>
2020-05-05 14:45:06 +0200

    Add project files.

---
+++ BTInventorySize/BTInventorySizeInit.cs
@@ -0,0 +1,28 @@
+﻿using BattleTech;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTInventorySize
+{
+    class BTInventorySizeInit
+    {
+        public static void Init(string dir, string sett)
+        {
+            var harmony = HarmonyInstance.Create("com.github.mcb5637.BTInventorySize");
+            //harmony.PatchAll(Assembly.GetExecutingAssembly());
+            harmony.Patch(typeof(MechComponentDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
+            harmony.Patch(typeof(AmmunitionBoxDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
+            harmony.Patch(typeof(HeatSinkDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
+            harmony.Patch(typeof(JumpJetDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
+            harmony.Patch(typeof(SpecialComponentDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
+            harmony.Patch(typeof(StatUpgradeDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
+            harmony.Patch(typeof(UpgradeDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
+            harmony.Patch(typeof(WeaponDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
+        }
+    }
+}
---
+++ BTInventorySize/MechComponentDef_InventorySize.cs
@@ -0,0 +1,19 @@
+﻿using BattleTech;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTInventorySize
+{
+    //[HarmonyPatch(typeof(MechComponentDef), "FromJSON")]
+    class MechComponentDef_InventorySize
+    {
+        public static void Postfix(MechComponentDef __instance)
+        {
+            Traverse.Create(__instance).Property("InventorySize").SetValue(1);
+        }
+    }
+}
