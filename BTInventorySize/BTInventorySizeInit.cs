bef70c4af665248d1f1e9167b2fb096d6c983121
mgrbo <mgrbo@web.de>
2020-11-28 16:50:07 +0100

    changed patch order to be executed after mechengineer, added version

--- BTInventorySize/BTInventorySizeInit.cs
+++ BTInventorySize/BTInventorySizeInit.cs
@@ -7,22 +7,26 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly:AssemblyVersion("1.0.1.0")]
+
 namespace BTInventorySize
 {
     class BTInventorySizeInit
     {
         public static void Init(string dir, string sett)
         {
-            var harmony = HarmonyInstance.Create("com.github.mcb5637.BTInventorySize");
+            HarmonyInstance harmony = HarmonyInstance.Create("com.github.mcb5637.BTInventorySize");
             //harmony.PatchAll(Assembly.GetExecutingAssembly());
-            harmony.Patch(typeof(MechComponentDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
-            harmony.Patch(typeof(AmmunitionBoxDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
-            harmony.Patch(typeof(HeatSinkDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
-            harmony.Patch(typeof(JumpJetDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
-            harmony.Patch(typeof(SpecialComponentDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
-            harmony.Patch(typeof(StatUpgradeDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
-            harmony.Patch(typeof(UpgradeDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
-            harmony.Patch(typeof(WeaponDef).GetMethod("FromJSON"), null, new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix")), null);
+            HarmonyMethod postfix = new HarmonyMethod(typeof(MechComponentDef_InventorySize).GetMethod("Postfix"));
+            postfix.after = new string[] { "MechEngineer.Features.AutoFix", "io.mission.modrepuation" };
+            harmony.Patch(typeof(MechComponentDef).GetMethod("FromJSON"), null, postfix, null);
+            harmony.Patch(typeof(AmmunitionBoxDef).GetMethod("FromJSON"), null, postfix, null);
+            harmony.Patch(typeof(HeatSinkDef).GetMethod("FromJSON"), null, postfix, null);
+            harmony.Patch(typeof(JumpJetDef).GetMethod("FromJSON"), null, postfix, null);
+            harmony.Patch(typeof(SpecialComponentDef).GetMethod("FromJSON"), null, postfix, null);
+            harmony.Patch(typeof(StatUpgradeDef).GetMethod("FromJSON"), null, postfix, null);
+            harmony.Patch(typeof(UpgradeDef).GetMethod("FromJSON"), null, postfix, null);
+            harmony.Patch(typeof(WeaponDef).GetMethod("FromJSON"), null, postfix, null);
         }
     }
 }
